2c15fd8223e0614a37374ee3c685cfa79e2324f6
Joshua Lyons <josh@lyonsdensoftware.com>
2016-02-24 19:23:59 -0600

    Test

--- src/com/ser215/rps/RPSClientApplication.java
+++ src/com/ser215/rps/RPSClientApplication.java
@@ -1,5 +1,5 @@
 /*
- * This is the main application class for the rock paper scissors game. Test
+ * This is the main application class for the rock paper scissors game. Test ME
  */
 package com.ser215.rps;
 
